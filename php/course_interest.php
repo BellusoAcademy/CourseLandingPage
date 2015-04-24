bd64cae94b4165d16f46d5e7da62a0c609423597
Richard Taimalie <richard@nzwebb.com>
2015-04-24 08:46:17 +0200

    Template Micro Site
    Images, scripts, pages of template

---
+++ php/course_interest.php
@@ -0,0 +1,18 @@
+<?php
+	if(isset($_POST['send'])){
+	$to = "courses@belluso.academy";
+	$from = $_POST['email'];
+	$first_name = $_POST['first_name'];
+	$last_name = $_POST['last_name'];
+	$company = $_POST['company'];
+	$location = $_POST['location'];
+	$phone = $_POST['phone'];
+	$subject = "Enquire after purchasing Italy trip ";
+	$message = "Name:" . " " . $first_name . " " . $last_name . "\n" . "Email:" . " " . $from . "\n" .
+	"Phone:" . " " . $phone . "\n" . "Company:" . " " . $company . "\n" . "Location:" . " " . $location . "\n\n" .
+	"Message:" . "\n\n " . $_POST['message'];
+	$headers = "From:" . $from;
+    mail($to,$subject,$message,$headers);
+	header('Location: ../index.html');	
+	}
+?>
